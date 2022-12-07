8e395bbbfd33e96d027d8c93501afe933fc1377d
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2022-12-07 20:52:34 +0100

    small patch of first solution

--- 1/solution.py
+++ 1/solution.py
@@ -2,15 +2,23 @@ import fileinput
 
 current_sum = 0
 greatest_sum = -1
+
+def update_greatest_sum():
+    global greatest_sum
+    global current_sum
+    if current_sum > greatest_sum:
+        greatest_sum = current_sum
+    elif current_sum < 0:
+        print("Error: current_sum < 0")
+
 for line in fileinput.input():
     if line == "\n":
-        if current_sum > greatest_sum:
-            greatest_sum = current_sum
-        elif current_sum < 0:
-            print("Error: current_sum < 0")
+        update_greatest_sum()
         current_sum = 0
         continue
     current_sum += int(line)
+
+update_greatest_sum()
 
 print(greatest_sum)
 
