044159d98564e0466ad5b01928d004cfa1429acb
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2023-01-03 12:37:08 +0100

    add some sample data and have something running for a long time

--- 19/advent-19-1/src/main.rs
+++ 19/advent-19-1/src/main.rs
@@ -1,13 +1,42 @@
 extern crate regex;
 
 use regex::Regex;
+use std::io::BufRead;
+
 
 fn main() {
-    println!("Hello, world!");
+    let blueprints = read_input();
+
+    let mut total_score = 0;
+
+    for blueprint in blueprints {
+        let print_score = iterate_solution(&blueprint, 0, 23, &Vec::new(), &vec![Resource {
+            resource_type: ResourceType::Ore,
+            amount: 1
+        }]);
+
+        print!("blueprint id {} score: {}", blueprint.id, print_score);
+        total_score += print_score * blueprint.id;
+    }
+
+    println!("total score: {}", total_score);
+}
+
+fn read_input() -> Vec<Blueprint> {
+    let stdin = std::io::stdin();
+    let mut lines = stdin.lock().lines();
+    let mut blueprints = Vec::new();
+    while let Some(Ok(line)) = lines.next() {
+        let blueprint = parse_blueprint(&line.trim());
+        blueprints.push(blueprint);
+
+    }
+    blueprints
 }
 
 #[derive(Debug)]
 #[derive(PartialEq)]
+#[derive(Copy, Clone)]
 enum ResourceType {
     Ore,
     Clay,
@@ -15,11 +44,16 @@ enum ResourceType {
     Geode
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Copy, Clone)]
 struct Resource {
     resource_type: ResourceType,
     amount: u32
 }
 
+#[derive(Debug)]
+#[derive(PartialEq)]
 struct Deal {
     price: Vec<Resource>,
     factory: ResourceType,
@@ -60,6 +94,107 @@ fn parse_deal(line: &str) -> Deal {
     }
 }
 
+fn is_deal_possible(deal: &Deal, resources: &Vec<Resource>) -> bool {
+    for price in &deal.price {
+        let mut found = false;
+        for resource in resources {
+            if resource.resource_type == price.resource_type {
+                found = true;
+                if resource.amount < price.amount {
+                    return false;
+                }
+            }
+        }
+        if !found {
+            return false;
+        }
+    }
+    true
+}
+
+fn get_geode_amount(resources: &Vec<Resource>) -> u32 {
+    for resource in resources {
+        if resource.resource_type == ResourceType::Geode {
+            return resource.amount;
+        }
+    }
+    0
+}
+
+fn grow_resources(resources: &Vec<Resource>, factories: &Vec<Resource>) -> Vec<Resource> {
+    let mut new_resources = resources.into_iter().map(|x| x.clone()).collect::<Vec<Resource>>();
+    for factory in factories {
+        let mut found = false;
+        for resource in &mut new_resources {
+            if resource.resource_type == factory.resource_type {
+                found = true;
+                resource.amount += factory.amount;
+                break;
+            }
+        }
+        if !found {
+            new_resources.push(factory.clone());
+        }
+    
+    }
+    new_resources
+}
+
+fn iterate_solution(blueprint: &Blueprint, index: u32, max_index: u32, resources: &Vec<Resource>, factories: &Vec<Resource>) -> u32 {
+
+    if index >= max_index {
+        return get_geode_amount(resources);
+    }
+    let possible_deals = blueprint.deals.iter().filter(|deal| is_deal_possible(deal, resources)).collect::<Vec<&Deal>>();
+
+    println!("index: {}. possible_deals: {}", index, possible_deals.len());
+    let mut best_score = iterate_solution(blueprint, index + 1, max_index, &grow_resources(resources, factories), &factories);
+    for deal in possible_deals {
+        let mut resources = perform_deal(&deal, &resources);
+        resources = grow_resources(&resources, &factories);
+        let new_factories = add_factory(&resources, deal.factory);
+        let deal_score = iterate_solution(blueprint, index + 1, max_index, &resources, &new_factories);
+        if deal_score > best_score {
+            best_score = deal_score;
+        }
+    }
+    best_score
+}
+
+fn add_factory(factories: &Vec<Resource>, factory: ResourceType) -> Vec<Resource> {
+    let mut new_factories = factories.into_iter().map(|x| x.clone()).collect::<Vec<Resource>>();
+    let mut found = false;
+    for f in &mut new_factories {
+        if f.resource_type == factory {
+            f.amount += 1;
+            found = true;
+        }
+    }
+    if !found {
+        new_factories.push(Resource {
+            resource_type: factory,
+            amount: 1,
+        });
+    }
+    new_factories
+}
+
+fn perform_deal(deal: &Deal, resources: &Vec<Resource>) -> Vec<Resource> {
+    let mut new_resources = resources.into_iter().map(|r| Resource {
+        resource_type: r.resource_type,
+        amount: r.amount,
+    }).collect::<Vec<Resource>>();
+
+    for price in &deal.price {
+        for resource in &mut new_resources {
+            if resource.resource_type == price.resource_type {
+                resource.amount -= price.amount;
+            }
+        }
+    }
+    new_resources
+}
+
 fn parse_price(line: &str) -> Vec<Resource> {
     line.split(" and ").map(|x| parse_single_price(x)).collect()
 }
@@ -91,6 +226,124 @@ fn parse_resource_type(line: &str) -> ResourceType {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_grow_resources() {
+        let resources = vec![
+            Resource {
+                resource_type: ResourceType::Ore,
+                amount: 1,
+            },
+            Resource {
+                resource_type: ResourceType::Clay,
+                amount: 2,
+            },
+        ];
+        let factories = vec![
+            Resource {
+                resource_type: ResourceType::Ore,
+                amount: 1,
+            },
+            Resource {
+                resource_type: ResourceType::Obsidian,
+                amount: 2,
+            },
+        ];
+        let new_resources = grow_resources(&resources, &factories);
+        assert_eq!(new_resources, vec![
+            Resource {
+                resource_type: ResourceType::Ore,
+                amount: 2,
+            },
+            Resource {
+                resource_type: ResourceType::Clay,
+                amount: 2,
+            },
+            Resource {
+                resource_type: ResourceType::Obsidian,
+                amount: 2,
+            },
+        ]);
+    }
+
+    #[test]
+    fn iterate_solution_returns_geode_amount_when_max_index_reached() {
+        let blueprint = Blueprint {
+            id: 1,
+            deals: vec![],
+        };
+        let resources = vec![Resource {
+            resource_type: ResourceType::Geode,
+            amount: 10,
+        }];
+        let factories = vec![];
+        let result = iterate_solution(&blueprint, 0, 0, &resources, &factories);
+        assert_eq!(result, 10);
+    }
+
+    #[test]
+    fn add_factory_test() {
+        let factories = vec![Resource {
+            resource_type: ResourceType::Ore,
+            amount: 1,
+        }];
+        let new_factories = add_factory(&factories, ResourceType::Ore);
+        assert_eq!(new_factories, vec![Resource {
+            resource_type: ResourceType::Ore,
+            amount: 2,
+        }]);
+        assert_eq!(factories, vec![Resource {
+            resource_type: ResourceType::Ore,
+            amount: 1,
+        }]);
+    }
+
+    #[test]
+    fn perform_deal_should_remove_resources() {
+        let deal = Deal {
+            price: vec![Resource { resource_type: ResourceType::Ore, amount: 1 }],
+            factory: ResourceType::Clay,
+        };
+        let resources = vec![Resource { resource_type: ResourceType::Ore, amount: 1 }];
+        let new_resources = perform_deal(&deal, &resources);
+        assert_eq!(new_resources, vec![Resource { resource_type: ResourceType::Ore, amount: 0 }]);
+    }
+
+    #[test]
+    fn should_say_deal_is_possible_when_enough_resources() {
+        let deal = Deal {
+            price: vec![Resource {
+                resource_type: ResourceType::Ore,
+                amount: 1,
+            }],
+            factory: ResourceType::Clay,
+        };
+
+        let resources = vec![Resource {
+            resource_type: ResourceType::Ore,
+            amount: 1,
+        }];
+
+        assert!(is_deal_possible(&deal, &resources));
+    }
+
+    #[test]
+    fn should_say_deal_is_not_possible_when_not_enough_resources() {
+        let deal = Deal {
+            price: vec![Resource {
+                resource_type: ResourceType::Ore,
+                amount: 1,
+            }],
+            factory: ResourceType::Clay,
+        };
+
+        let resources = vec![Resource {
+            resource_type: ResourceType::Clay,
+            amount: 1,
+        }];
+
+        assert!(!is_deal_possible(&deal, &resources));
+    }
 
     #[test]
     fn should_parse_blueprint() {
